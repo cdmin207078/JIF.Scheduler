dca1d6e3cff06fb96f26e9d30299bfa16174d22c
天雨流芳 <ning.chen@zeropartner.com>
2017-02-27 19:01:11 +0800

    init

---
+++ code/JIF.Scheduler.Web/Scripts/Layout.js
@@ -0,0 +1,8 @@
+﻿
+$(function () {
+    //iCheck for checkbox and radio inputs
+    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue'
+    });
+});
